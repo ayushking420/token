6e491716fe9f0f50b5c771d0a213253da0c0ca8e
Xmãrty Ãyûsh Kîñg <138370370+ayushking420@users.noreply.github.com>
2026-01-14 11:35:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def login():
     return render_template("login.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
